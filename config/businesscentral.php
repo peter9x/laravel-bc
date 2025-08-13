dd1fe6d4818af5c28f6afd1c1f3dbb78b9b85091
Pedro Teixeira <pedro.eletronik@gmail.com>
2025-08-13 18:31:59 +0100

    Update businesscentral.php

--- config/businesscentral.php
+++ config/businesscentral.php
@@ -1,10 +1,14 @@
 <?php
 
 return [
-    'client_id' => env('BC_CLIENT_ID'),
-    'client_secret' => env('BC_CLIENT_SECRET'),
-    'tenant_id' => env('BC_TENANT_ID'),
-    'environment' => env('BC_ENVIRONMENT', 'sandbox'), // or production
-    'company_id' => env('BC_COMPANY_ID'),
+    'connections' => [
+        'default' => [
+            'client_id' => env('BC_CLIENT_ID'),
+            'client_secret' => env('BC_CLIENT_SECRET'),
+            'tenant_id' => env('BC_TENANT_ID'),
+            'environment' => env('BC_ENVIRONMENT', 'local'), // or production
+            'company_id' => env('BC_COMPANY_ID'),
+        ]
+    ],
     'api_url' => env('BC_API_URL', 'https://api.businesscentral.dynamics.com/v2.0/'),
 ];
